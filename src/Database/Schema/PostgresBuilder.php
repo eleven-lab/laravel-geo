9cfd717f847713b86c8d056b3f034006156cbec6
Lorenzo Giustozzi <giustozzi@lorenzo.im>
2017-07-18 19:17:22 +0200

    Fix multi-schema postgres connections handling

--- src/Database/Schema/PostgresBuilder.php
+++ src/Database/Schema/PostgresBuilder.php
@@ -2,15 +2,28 @@
 
 namespace ElevenLab\GeoLaravel\Database\Schema;
 
-if(version_compare(app()->version(), "5.2.31") < 0){
-    class PostgresBuilder extends \Illuminate\Database\Schema\Builder
-    {
-        use GeoBuilder;
-    }
+class PostgresBuilder extends \Illuminate\Database\Schema\Builder
+{
 
-}else{
-    class PostgresBuilder extends \Illuminate\Database\Schema\PostgresBuilder
+    use GeoBuilder;
+    /**
+     * Determine if the given table exists.
+     *
+     * @param  string  $table
+     * @return bool
+     */
+    public function hasTable($table)
     {
-        use GeoBuilder;
+        if (is_array($schema = $this->connection->getConfig('schema'))) {
+            $schema = head($schema);
+        }
+
+        $schema = $schema ? $schema : 'public';
+
+        $table = $this->connection->getTablePrefix().$table;
+
+        return count($this->connection->select(
+                $this->grammar->compileTableExists(), [$schema, $table]
+            )) > 0;
     }
 }
